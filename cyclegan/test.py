b0e60e44507a702f6f310af4ecb627fed90d9a6a
WW <57519673+WeiWang1205@users.noreply.github.com>
2020-07-29 17:33:41 -0700

    Add files via upload

---
+++ cyclegan/test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import os
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import torch
+
+from models import Generator
+from datasets import ImageDataset
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
+parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
+parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
+parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
+parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
+parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
+parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
+parser.add_argument('--cuda', action='store_true', help='use GPU computation')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+opt = parser.parse_args()
+print(opt)
+
+
+rootA = "../dataset/monet_field_data"
+rootB = "../dataset/landscape_test"
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+###### Definition of variables ######
+# Networks
+netG_A2B = Generator(opt.input_nc, opt.output_nc)
+netG_B2A = Generator(opt.output_nc, opt.input_nc)
+
+if opt.cuda:
+    netG_A2B.cuda()
+    netG_B2A.cuda()
+
+# Load state dicts
+netG_A2B.load_state_dict(torch.load('output/netG_A2B.pth'))
+netG_B2A.load_state_dict(torch.load('output/netG_B2A.pth'))
+
+# Set model's test mode
+netG_A2B.eval()
+netG_B2A.eval()
+
+# Inputs & targets memory allocation
+Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
+input_A = Tensor(opt.batchSize, opt.input_nc, opt.size, opt.size)
+input_B = Tensor(opt.batchSize, opt.output_nc, opt.size, opt.size)
+
+# Dataset loader
+transforms_ = [ transforms.Resize([opt.size, opt.size], Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
+dataloader = DataLoader(ImageDataset(rootA, rootB, transforms_=transforms_, unaligned=False), 
+                        batch_size=opt.batchSize, shuffle=False, num_workers=opt.n_cpu)
+###################################
+
+###### Testing######
+
+# Create output dirs if they don't exist
+if not os.path.exists('output/A'):
+    os.makedirs('output/A')
+if not os.path.exists('output/B'):
+    os.makedirs('output/B')
+
+for i, batch in enumerate(dataloader):
+    # Set model input
+    real_A = Variable(input_A.copy_(batch['A']))
+    real_B = Variable(input_B.copy_(batch['B']))
+    
+    # Save image files
+    save_image(0.5*(real_A+1.0), 'output/A/real{}.png'.format(i))
+    save_image(0.5*(real_B+1.0), 'output/B/real{}.png'.format(i))
+
+    # Generate output
+    fake_B = 0.5*(netG_A2B(real_A).data + 1.0)
+    fake_A = 0.5*(netG_B2A(real_B).data + 1.0)
+
+    # Save image files
+    save_image(fake_A, 'output/A/fake{}.png'.format(i))
+    save_image(fake_B, 'output/B/fake{}.png'.format(i))
+
+    sys.stdout.write('\rGenerated images %04d of %04d' % (i+1, len(dataloader)))
+
+sys.stdout.write('\n')
+###################################
